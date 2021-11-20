7b63eac8cefbc9f2df184138237087d42aab9bd9
gabrielmenezes30 <menezesg792@gmailcom>
2021-11-20 18:03:15 -0300

    página welcome layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,9 @@
         <title>Hanie Resale</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap" rel="stylesheet">
 
         <!-- Link CSS -->
 
@@ -20,68 +22,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     </head>
-    {{-- <body>
-        <div class="flex-center position-ref full-height">
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Hanie Resale
-                </div>
-            </div>
-            <!--Eu passei por aqui-->
-
-            <!-- @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif -->
-
-            @if(Route::has('login'))
-            <div class="container-botoes links">
-                @auth
-                <a href="{{ url('/home') }}">
-                    <button class="btnHome">
-                    <i class="fas fa-home"></i>
-                        Entrar
-                    </button>
-                </a>
-                @else
-                <a href="{{ route('login') }}">
-                <button class="btn btn1">
-                    Login
-                </button>
-                </a>
-
-                @if(Route::has('register'))
-
-                    <a href="{{ route('register') }}">
-                        <button class="btn btn2">
-                            Cadastrar
-                        </button>
-                    </a>
-                @endif
-            @endauth
-            </div>
-        @endif
-        </div>
-    </body> --}}
     <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">HanieResale</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
                     {{-- @if (Route::has('login'))
                     <div class="top-right links">
                         @auth
@@ -135,12 +76,8 @@
                 @endauth
                 </div>
             @endif --}}
-                </div>
-              </div>
-            </div>
-        </nav>
 
-        <section>
+        <section id="primeriaSection">
             <div class="d-flex flex-row align-items-center justify-content-around container-fluid containerHome">
                 <div class="textoHome">
                     <p>Bem vindo a <strong>Hanie Resale</strong> </p>
@@ -178,8 +115,80 @@
                     <img src="{{asset('assets/images/svg/svg-home.svg')}}" alt="" width="600">
                 </div>
             </div>
+            <div class="d-flex flex-row justify-content-center arrow">
+                <i class="fas fa-arrow-down"></i>
+            </div>
         </section>
 
+        <section id="segundaSection">
+            <div class="container p-5">
+                <div class="d-flex flex-column">
+                    <div class="text-center">
+                        <h1 class="textoPecas">Peças em disposição</h1>
+                    </div>
+                    <div class="boxInfo">
+                        <h1>Vendas</h1>
+                    </div>
+                    <div class="d-flex flex-row-reverse boxCard">
+                        <div class="card teste" style="width: 18rem;">
+                            <img src="{{asset('assets/images/roupa.jpg')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                          </div>
+                          <div class="card teste" style="width: 18rem;">
+                            <img src="{{asset('assets/images/roupa.jpg')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                          </div>
+                          <div class="card teste" style="width: 18rem;">
+                            <img src="{{asset('assets/images/roupa.jpg')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                          </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </section>
+        <section id="terceiraSection">
+            <div class="container p-5">
+                <div class="d-flex flex-column">
+                    <div class="boxInfo">
+                        <h1>Doações</h1>
+                    </div>
+                    <div class="d-flex flex-row-reverse boxCard">
+                        <div class="card teste" style="width: 18rem;">
+                            <img src="{{asset('assets/images/roupa.jpg')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                          </div>
+                          <div class="card teste" style="width: 18rem;">
+                            <img src="{{asset('assets/images/roupa.jpg')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                          </div>
+                          <div class="card teste" style="width: 18rem;">
+                            <img src="{{asset('assets/images/roupa.jpg')}}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer>
+            <div>
+                <h5 class="text-white fs-1">Hanie Resale</h5>
+            </div>
+        </footer>
     </body>
 
         <!-- Font Awesome -->
